b8d7797d4c81d1b89933dce57a6d069110d84300
Don286 <don.burgess286@gmail.com>
2016-06-17 02:09:52 -0400

    android skeleton test

--- nativescript-rabbitmq.android.js
+++ nativescript-rabbitmq.android.js
@@ -1,9 +1,16 @@
 "use strict";
-var RabbitMQ = (function () {
-    function RabbitMQ(test) {
-        this.test = "Hello Android";
-        this.test2 = test;
+var Client = (function () {
+    function Client() {
+        this.test = "Test";
+        this.AMQP = com.rabbitmq.client.AMQP;
+        this.Channel = com.rabbitmq.client.Channel;
+        this.Connection = com.rabbitmq.client.Connection;
+        this.ConnectionFactory = com.rabbitmq.client.ConnectionFactory;
+        this.Consumer = com.rabbitmq.client.Consumer;
+        this.DefaultConsumer = com.rabbitmq.client.DefaultConsumer;
+        this.Envelope = com.rabbitmq.client.Envelope;
+        this.QueueingConsumer = com.rabbitmq.client.QueueingConsumer;
     }
-    return RabbitMQ;
+    return Client;
 }());
-exports.RabbitMQ = RabbitMQ;
+exports.Client = Client;
--- src/nativescript-rabbitmq.android.ts
+++ src/nativescript-rabbitmq.android.ts
@@ -1,10 +1,25 @@
 declare var com: any;
 
-export class RabbitMQ {
-    test: string = "Hello Android";
-    test2: string;
+export class Client {
+    test: string = "Test";
+    
+    AMQP: Function;
+    Channel: Function;
+    Connection: Function;
+    ConnectionFactory: Function;
+    Consumer: Function;
+    DefaultConsumer: Function;
+    Envelope: Function;
+    QueueingConsumer: Function;
 
-    constructor(test){
-        this.test2 = test;
+    constructor(){
+        this.AMQP = com.rabbitmq.client.AMQP;
+        this.Channel = com.rabbitmq.client.Channel;
+        this.Connection = com.rabbitmq.client.Connection;
+        this.ConnectionFactory = com.rabbitmq.client.ConnectionFactory;
+        this.Consumer = com.rabbitmq.client.Consumer;
+        this.DefaultConsumer = com.rabbitmq.client.DefaultConsumer;
+        this.Envelope = com.rabbitmq.client.Envelope;
+        this.QueueingConsumer = com.rabbitmq.client.QueueingConsumer;
     }
 }
