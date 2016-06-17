9be691fab072f408df1a479b8d1e41ad2a5917b7
Nathanael Anderson <nathan@master-technology.com>
2016-06-17 02:33:55 -0500

    Initial Android version.

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -2,9 +2,23 @@ import * as observable from 'data/observable';
 import * as pages from 'ui/page';
 import {HelloWorldModel} from './main-view-model';
 
+var rabbitmq = require('nativescript-rabbitmq');
+
 // Event handler for Page "loaded" event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
     // Get the event sender
     var page = <pages.Page>args.object;
     page.bindingContext = new HelloWorldModel();
+
+    var factory = new rabbitmq.ConnectionFactory();
+    factory.setUsername("me");
+    factory.setPassword("you");
+    var conn = factory.newConnection();
+    var channel = conn.createChannel();
+
+    // Do Whatever with the channel
+
+    channel.close();
+    conn.close();
+
 }
--- nativescript-rabbitmq.android.js
+++ nativescript-rabbitmq.android.js
@@ -1,16 +1,70 @@
 "use strict";
-var Client = (function () {
-    function Client() {
-        this.test = "Test";
-        this.AMQP = com.rabbitmq.client.AMQP;
-        this.Channel = com.rabbitmq.client.Channel;
-        this.Connection = com.rabbitmq.client.Connection;
-        this.ConnectionFactory = com.rabbitmq.client.ConnectionFactory;
-        this.Consumer = com.rabbitmq.client.Consumer;
-        this.DefaultConsumer = com.rabbitmq.client.DefaultConsumer;
-        this.Envelope = com.rabbitmq.client.Envelope;
-        this.QueueingConsumer = com.rabbitmq.client.QueueingConsumer;
+
+// Typically we want to wrap the native Java class in our own class; this makes extending it a lot easier
+var Connection = function(nativeConnection) {
+    this._connection = nativeConnection;
+};
+Connection.prototype.createChannel = function() {
+    // Because we wrapped this class, we can add extra error checks or other helper functions
+    if (!this._connection) {
+        throw new Error("Attempting to create Channel on closed Connection!");
     }
-    return Client;
-}());
-exports.Client = Client;
+    // But you don't actually have to wrap them, you can actually return the native
+    // Java Class like I'm doing in this example.   (However my preference would be to wrap it)
+    return this._connection.createChannel();
+};
+Connection.prototype.close = function() {
+    this._connection.close();
+    this._connection = null;
+};
+// TODO: Add whatever other "Connection" helper classes we need.
+
+
+
+
+var ConnectionFactory = function() {
+    this._factory = new com.rabbitmq.client.Connection.ConnectionFactory();
+};
+
+ConnectionFactory.prototype.setUsername = function(username)
+{
+   this._factory.setUsername(username);
+};
+
+ConnectionFactory.prototype.getUsername = function()
+{
+    return this._factory.getUsername().toString();
+};
+
+// ---------------------------------------------------------------------------------
+// My Personal preference is actually combine the two of them into a single function
+// But this is really your choice how you want to write your wrapper; you can follow it exactly
+// like the original library; and do separate getters/setters.
+// ---------------------------------------------------------------------------------
+ConnectionFactory.prototype.username = function(username) {
+    if (arguments.length) {
+        this._factory.setUsername(username);
+    } else {
+        return this._factory.getUsername();
+    }
+};
+
+
+ConnectionFactory.prototype.setPassword = function(password) {
+    this._factory.setPassword(password);
+};
+
+ConnectionFactory.prototype.getPassword = function() {
+    this._factory.getPassword();
+};
+
+// TODO: Should add the rest of the factory prototypes like set/get port, etc.
+
+ConnectionFactory.prototype.newConnection = function() {
+    return new Connection(this._factory.newConnection());
+};
+
+exports.ConnectionFactory = ConnectionFactory;
+
+
+// TODO add any other classes that need to be exposed
