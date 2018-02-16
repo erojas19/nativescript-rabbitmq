2a0b69b2f1a88b9b6d1442d4f33a017d39842445
Efrain Rojas <erojas@zenlabs.net>
2018-02-16 14:16:54 -0400

    update new

--- nativescript-rabbitmq.android.js
+++ nativescript-rabbitmq.android.js
@@ -5,11 +5,11 @@ var Client = (function () {
         this.AMQP = com.rabbitmq.client.AMQP;
         this.Channel = new com.rabbitmq.client.Channel();
         this.Connection =  new com.rabbitmq.client.Connection();
-        this.ConnectionFactory =  new com.rabbitmq.client.ConnectionFactory();
-        this.Consumer =  new com.rabbitmq.client.Consumer();
-        this.DefaultConsumer =  new com.rabbitmq.client.DefaultConsumer();
-        this.Envelope =  new com.rabbitmq.client.Envelope();
-        this.QueueingConsumer =  new com.rabbitmq.client.QueueingConsumer();
+        this.ConnectionFactory = new com.rabbitmq.client.ConnectionFactory();
+        this.Consumer = com.rabbitmq.client.Consumer;
+        this.DefaultConsumer = com.rabbitmq.client.DefaultConsumer;
+        this.Envelope = com.rabbitmq.client.Envelope;
+        this.QueueingConsumer = com.rabbitmq.client.QueueingConsumer;
     }
     return Client;
 }());
